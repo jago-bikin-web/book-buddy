6ffc74f2dabab7ad1d20427d6b0929c726ce0e34
NadhiraWidyaniswari <nadhira.wiedyakusumo@gmail.com>
2023-10-29 17:53:00 +0700

    deploy sementara

--- eventbuddy/urls.py
+++ eventbuddy/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('delete/<int:id>', delete_event, name='delete_event'),
     path('regis-event/<int:id>', regis_event, name='regis_event'),
     path('attendees/<int:id>', show_attendees, name='show_attendees'),
+    path('get-event/', get_event_json, name='get_event_json'),
+    path('create-event-ajax/', add_event_ajax, name='add_event_ajax'),
+    path('get-books-ID/<int:id>', get_book_json, name='get_book_json'),
 ]
--- eventbuddy/views.py
+++ eventbuddy/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from book.models import Book
 from eventbuddy.models import Event
@@ -9,6 +9,7 @@ from eventbuddy.forms import EventForm
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from main.models import Profile
+from django.views.decorators.csrf import csrf_exempt
 
 @login_required(login_url='main:login')
 def show_eventbuddy(request):
@@ -20,7 +21,7 @@ def show_eventbuddy(request):
         'profile_picture': profile_picture,
         'events' : events,
     }
-    return render(request, "eventbuddy.html", context)
+    return render(request, "ebuddy.html", context)
 
 @login_required(login_url='main:login')
 def create_event(request):
@@ -31,6 +32,8 @@ def create_event(request):
         if form.is_valid() and request.method == "POST":
             event = form.save(commit=False)
             event.user = request.user
+            selected_book_id = request.POST.get('book')  # Pastikan 'book' sesuai dengan nama field pada formulir
+            event.book = Book.objects.get(pk=selected_book_id)
             event.save()
             return HttpResponseRedirect(reverse('eventbuddy:show_eventbuddy'))
 
@@ -45,6 +48,10 @@ def create_event(request):
 def get_event_json(request):
     events = Event.objects.all()
     return HttpResponse(serializers.serialize('json', events))
+
+def get_book_json(request, id):
+    book = Book.objects.get(pk = id)
+    return HttpResponse(serializers.serialize('json', [book]))
 
 @login_required(login_url='main:login')
 def edit_event(request, id):
@@ -86,7 +93,7 @@ def regis_event(request, id):
     return HttpResponseRedirect(reverse('eventbuddy:show_eventbuddy'))
 
 
-@login_required(login_url='main:lgin')       
+@login_required(login_url='main:login')       
 def show_attendees(request, id):
     events = Event.objects.get(pk = id)
     user = Profile.objects.get(user=request.user)
@@ -100,5 +107,21 @@ def show_attendees(request, id):
         return render(request, "attendees.html", context)
     else:
         return HttpResponseRedirect(reverse('eventbuddy:show_eventbuddy'))
+    
+...
+@csrf_exempt
+def add_event_ajax(request, id):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        date = request.POST.get("date")
+        description = request.POST.get("description")
+        user = request.user
+        books = Book.objects.get(pk=request.POST.get("idBooks"))
 
+        new_product = Event(book=books,user=user, name=name, date=date, description=description)
+        new_product.save()
+
+        return HttpResponse(b"CREATED", status=201)
+
+    return HttpResponseNotFound()
 # Create your views here.
