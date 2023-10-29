c56b771972ed4804a01be511ec1775a9cb33452c
NadhiraWidyaniswari <nadhira.wiedyakusumo@gmail.com>
2023-10-29 23:17:16 +0700

    feat: bismillah

--- eventbuddy/urls.py
+++ eventbuddy/urls.py
@@ -5,7 +5,7 @@ app_name = 'eventbuddy'
 
 urlpatterns = [
     path('', show_eventbuddy, name='show_eventbuddy'),
-    path('create-event', create_event, name='create_event'),
+    path('create-event/', create_event, name='create_event'),
     path('edit-event/<int:id>', edit_event, name='edit_event'),
     path('delete/<int:id>', delete_event, name='delete_event'),
     path('regis-event/<int:id>', regis_event, name='regis_event'),
