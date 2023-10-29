cfa06194d9b065c19a464d28858e34a664632763
NadhiraWidyaniswari <nadhira.wiedyakusumo@gmail.com>
2023-10-29 13:58:15 +0700

    feat menyelesaikan modul tapi belum ajax

--- eventbuddy/urls.py
+++ eventbuddy/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', show_eventbuddy, name='show_eventbuddy'),
     path('create-event', create_event, name='create_event'),
     path('edit-event/<int:id>', edit_event, name='edit_event'),
+    path('delete/<int:id>', delete_event, name='delete_event'),
+    path('regis-event/<int:id>', regis_event, name='regis_event'),
+    path('attendees/<int:id>', show_attendees, name='show_attendees'),
 ]
--- eventbuddy/views.py
+++ eventbuddy/views.py
@@ -7,41 +7,98 @@ from django.core import serializers
 from django.http import HttpResponseRedirect
 from eventbuddy.forms import EventForm
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from main.models import Profile
 
+@login_required(login_url='main:login')
 def show_eventbuddy(request):
     events = Event.objects.all()
+    user = Profile.objects.get(user=request.user)
+    profile_picture = user.profile_picture
     context = {
+        'user': user.full_name,
+        'profile_picture': profile_picture,
         'events' : events,
     }
     return render(request, "eventbuddy.html", context)
 
+@login_required(login_url='main:login')
 def create_event(request):
     form = EventForm(request.POST or None)
+    books = Book.objects.all()
+    user = Profile.objects.get(user=request.user)
+    if user.is_member():
+        if form.is_valid() and request.method == "POST":
+            event = form.save(commit=False)
+            event.user = request.user
+            event.save()
+            return HttpResponseRedirect(reverse('eventbuddy:show_eventbuddy'))
 
-    if form.is_valid() and request.method == "POST":
-        form.save()
+        context = {
+            'form': form,
+            'books': books,
+            }
+        return render(request, "create_event.html", context)
+    else:
         return HttpResponseRedirect(reverse('eventbuddy:show_eventbuddy'))
-
-    context = {'form': form}
-    return render(request, "create_event.html", context)
 
 def get_event_json(request):
     events = Event.objects.all()
     return HttpResponse(serializers.serialize('json', events))
 
+@login_required(login_url='main:login')
 def edit_event(request, id):
     # Get product berdasarkan ID
     events = Event.objects.get(pk = id)
+    books = Book.objects.all()
+    user = Profile.objects.get(user=request.user)
 
     # Set product sebagai instance dari form
-    form = EventForm(request.POST or None, instance=events)
+    
+    if request.user == events.user and user.is_member():
+        form = EventForm(request.POST or None, instance=events)
+        if form.is_valid() and request.method == "POST":
+            # Simpan form dan kembali ke halaman awal
+            form.save()
+            return HttpResponseRedirect(reverse('eventbuddy:show_eventbuddy'))
 
-    if form.is_valid() and request.method == "POST":
-        # Simpan form dan kembali ke halaman awal
-        form.save()
+        context = {'form': form,
+                'books': books,
+                }
+        return render(request, "edit_event.html", context)
+    else:
         return HttpResponseRedirect(reverse('eventbuddy:show_eventbuddy'))
 
-    context = {'form': form}
-    return render(request, "edit_event.html", context)
+@login_required(login_url='main:login')
+def delete_event(request, id):
+    events = Event.objects.get(pk = id)
+    user = Profile.objects.get(user=request.user)
+    if request.user == events.user and user.is_member():
+        events.delete()
+    return HttpResponseRedirect(reverse('eventbuddy:show_eventbuddy'))
+
+@login_required(login_url='main:login')
+def regis_event(request, id):
+    events = Event.objects.get(pk = id)
+    if request.user != events.user:
+        profile = request.user.profile
+        events.participant.add(profile)
+    return HttpResponseRedirect(reverse('eventbuddy:show_eventbuddy'))
+
+
+@login_required(login_url='main:lgin')       
+def show_attendees(request, id):
+    events = Event.objects.get(pk = id)
+    user = Profile.objects.get(user=request.user)
+    if request.user == events.user and user.is_member():
+        participants = events.participant.all()
+        count = participants.count()
+        context = {
+            'participants': participants,
+            'count' : count, 
+        }
+        return render(request, "attendees.html", context)
+    else:
+        return HttpResponseRedirect(reverse('eventbuddy:show_eventbuddy'))
 
 # Create your views here.
