134a6a03aa6ad16080a63f5a9807a8ebae5f51c0
Restu Ahmad Ar Ridho <restuaarridho@gmail.com>
2023-12-06 17:15:49 +0700

    fix: register flutter

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+
+class AppTest(TestCase):
+    # ----------------- Test Authenticated ----------------- #
+    def test_aman(self):
+        response = self.client.post(path=reverse("authentication:register"), data={
+            "full_name": "test",
+            "username": "test",
+            "email": "test",
+            "status": "M",
+            "password1": "test",
+            "password2": "test"
+        }, content_type="application/json")
+
+        self.assertEqual(response.json(), {
+            "username": "test",
+            "status": True,
+            "message": "Register sukses!"
+        })
