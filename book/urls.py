4742ffaf213481a21be454efe4b4e7e198f311ca
Restu Ahmad Ar Ridho <restuaarridho@gmail.com>
2023-12-12 14:51:58 +0700

    feat: views untuk menerima update buku

--- book/urls.py
+++ book/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('get-books/', get_books, name='get_book'),
     path('get-random/', get_random_books, name='get_random_books'),
     path('search-books/', get_search_books, name='get_search_books'),
+    path('get-50-books/', get_50_random_books, name='get_50_random_books'),
 ]
